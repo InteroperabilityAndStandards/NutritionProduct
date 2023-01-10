7a961e4e7fe706eaec33b645a669aa5648b22c93
James Allain <james.allain@gmail.com>
2023-01-10 08:54:04 -0500

    changed request timeout to 15 everywhere

--- src/components/get_foods.py
+++ src/components/get_foods.py
@@ -8,11 +8,11 @@ def main(use_server: bool, base_url: str, food_name: str):
     with st.spinner():
         # if st.session_state.product_name != food_name:
         if use_server:
-            response = requests.get(f"{base_url}/food/{food_name}", timeout=10)
+            response = requests.get(f"{base_url}/food/{food_name}", timeout=15)
         else:
             response = requests.get(
                 f"https://api.nal.usda.gov/fdc/v1/food/{food_name}?api_key=DEMO_KEY",
-                timeout=10,
+                timeout=15,
             )
     if response and response.status_code == 200:
         st.session_state.product_name = food_name
--- src/components/get_supplements.py
+++ src/components/get_supplements.py
@@ -10,12 +10,12 @@ def main(use_server: bool, base_url: str, supplement_name: str):
         if use_server:
             response = requests.get(
                 f"{base_url}/supplement/{supplement_name}",
-                timeout=10,
+                timeout=15,
             )
         else:
             response = requests.get(
                 f"https://api.ods.od.nih.gov/dsld/v8/label/{supplement_name}",
-                timeout=10,
+                timeout=15,
             )
     if response and response.status_code == 200:
         st.session_state.product_name = supplement_name
